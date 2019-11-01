5139a3ead1c07c9a8b875281c0937fc65c781fb2
Filip Balucha <balucha.filip2@gmail.com>
2019-11-01 15:16:05 +0000

    Implement a way for adding accelerometers to the navigation system

--- navigation.cpp
+++ navigation.cpp
@@ -18,28 +18,28 @@ Navigation::~Navigation()
 void Navigation::add_accelerometer(Accelerometer *a)
 {
   /// TASK 7 (PERSON B) ////////////////////////////////////////////////////////
-  
+  this->accelerometers.push_back(a);
   /// END OF TASK 7B ///////////////////////////////////////////////////////////
 }
 
 double Navigation::get_acceleration()
 {
   double a = 0;
-  for (Accelerometer* accelerometer : this->accelerometers)
+  for (Accelerometer *accelerometer : this->accelerometers)
     a += accelerometer->get_acceleration();
-  return a/this->accelerometers.size();
+  return a / this->accelerometers.size();
 }
 
 double Navigation::get_velocity()
 {
-   /// TASK 8 (PERSON A) ///////////////////////////////////////////////////////
-  
+  /// TASK 8 (PERSON A) ///////////////////////////////////////////////////////
+
   /// END OF TASK 8A ///////////////////////////////////////////////////////////
 }
 
 double Navigation::get_position()
 {
   /// TASK 8 (PERSON B) ////////////////////////////////////////////////////////
-  
+
   /// END OF TASK 8B ///////////////////////////////////////////////////////////
 }
